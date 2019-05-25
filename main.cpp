edbb8085b04961dcb791a42696292954d75cdedd
paulofreitasricardo <48597008+paulofreitasricardo@users.noreply.github.com>
2019-05-24 21:46:42 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+using namespace std;
+
+
+class Pizza {
+private:
+	string sabor;
+public:
+	Pizza() {}
+	Pizza(string sabor)
+	{
+		this->sabor = sabor;
+	} 
+    void setSabor(string sabor);
+    void print();
+
+};
+
+
+class Lista {
+private:
+    Pizza **pizza;  //1 declarar o vetor de vetores 
+    int TAM;
+    int n;             // tamanho corrente da lista
+    bool posicaoValida(int);
+    bool vazia();
+public:
+    Lista (): Lista(100)  { }
+
+    Lista(int tam) {
+        n = 0;
+        TAM = tam;
+        pizza = new Pizza*[TAM];  //aloca a memoria para o vetor 
+    }
+    void insere(Pizza);
+    void print();
+};
+
+
+//-----------------------------------------------------------------------------------------------
+
+void Pizza::setSabor(string sabor){
+    this->sabor = sabor;
+
+}
+
+
+void Pizza::print(){
+    cout << sabor << "";
+}
+
+
+
+
+// ------------------- LISTA
+void Lista::insere(Pizza x) {
+    if (n < TAM) {
+        n++;
+        Pizza *ptr = x;
+        //pizza[n] = x;
+        pizza[n] = ptr;
+        //cout << "" << x;
+
+    }
+    else {
+        cout << "Lista cheia!\n";
+    }
+}
+
+
+
+
+
+
+
+void Lista::print() {
+    for (int i = 1; i <= n; i++)  {
+        pizza[i]->print();
+        cout << endl;
+    }
+}
+
+
+//---------------------------------------------------------------------------------------------------
+
+/*void testaInsere(Lista l) {
+    l.print();
+    cout << "\n\n";
+    cout << "----------------------\n";
+}*/
+
+
+
+int main(int argc, const char * argv[]) {
+
+   
+    const int tam = 10;
+    Lista l(tam);
+    Pizza a("Queijo1");
+
+    l.insere(a);
+
+    Pizza y;
+    cout << "\n\n";
+    l.print();
+    cout << "\n\n";
+
+    //testaInsere(l);
+}
